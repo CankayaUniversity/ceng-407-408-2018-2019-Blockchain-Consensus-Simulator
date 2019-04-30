c9208dcbeb48331dc325bf4fa4f32e6db5cf54f8
ercnmustafa <44052078+ercnmustafa@users.noreply.github.com>
2019-04-30 14:42:00 +0300

    Add files via upload

---
+++ ClassDefination/Network.java
@@ -0,0 +1,48 @@
+
+public class Network {
+	private int Network_id;
+	private int []edge_id;
+	
+	public Network()
+	{
+		Network_id=0;
+		edge_id=null;
+	}
+	public void AddNewNode()
+	{
+		Node asd= new Node();
+	}
+	public void SelectNode()
+	{
+		
+	}
+	public void ChoosePOW(int hashRate)
+	{
+		
+	}
+	public void ChoosePOS()
+	{
+		
+	}
+	public void DeleteNetwork()
+	{
+		
+	}
+	public void SetEdge(int number)
+	{
+		
+	}
+	public int GetEdge(int i)
+	{
+		return edge_id[i];
+	}
+	public void SetNetworkID(int number)
+	{
+		Network_id=number;
+	}
+	public int GetNetworkID()
+	{
+		return Network_id;
+	}
+	
+}
